113fed8ac4eb69e14ae5f6ff2cb749c85dfd0ab9
isakhno <sakhno.yaroslav.13.95@gamil.com>
2025-05-21 09:41:33 +0600

    создание README.md

--- src/main/java/sakhno/psup/config_server/ConfigServerApplication.java
+++ src/main/java/sakhno/psup/config_server/ConfigServerApplication.java
@@ -2,7 +2,13 @@ package sakhno.psup.config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * Данный сервис выступает как конфигурационный сервер, через который остальные сервисы могут получить
+ * настройки для запуска.
+ */
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
